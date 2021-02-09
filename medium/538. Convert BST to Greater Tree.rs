0653bd456fe87105e0132efd32b21e9a54d05dc8
Satyarth Agrahari <satyarthagrahari@gmail.com>
2021-02-09 20:26:05 +0530

    Create 538. Convert BST to Greater Tree.rs

---
+++ medium/538. Convert BST to Greater Tree.rs
@@ -0,0 +1,42 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::borrow::Borrow;
+
+impl Solution {
+    fn update_node(node: &Option<Rc<RefCell<TreeNode>>>, val: i32) -> i32 {
+        if let Some(node) = node.borrow() {
+            let mut node = node.as_ref().borrow_mut();
+            let rval = Self::update_node(&node.right, val);
+            let lval = Self::update_node(&node.left, val + rval + node.val);
+            let to_return = rval + lval + node.val;
+            node.val += rval + val;
+            to_return
+        } else {
+            0
+        }
+    }
+
+    pub fn convert_bst(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::update_node(&root, 0);
+        root
+    }
+}
