1c52e3b5071e0d39c1a77df4775d546649f112b8
Satyarth Agrahari <satyarthagrahari@gmail.com>
2021-02-09 20:30:21 +0530

    Update 538. Convert BST to Greater Tree.rs

--- medium/538. Convert BST to Greater Tree.rs
+++ medium/538. Convert BST to Greater Tree.rs
@@ -23,16 +23,15 @@ use std::borrow::Borrow;
 
 impl Solution {
     fn update_node(node: &Option<Rc<RefCell<TreeNode>>>, val: i32) -> i32 {
+        let mut to_return = 0;
         if let Some(node) = node.borrow() {
             let mut node = node.as_ref().borrow_mut();
             let rval = Self::update_node(&node.right, val);
             let lval = Self::update_node(&node.left, val + rval + node.val);
-            let to_return = rval + lval + node.val;
+            to_return = rval + lval + node.val;
             node.val += rval + val;
-            to_return
-        } else {
-            0
-        }
+        } 
+        to_return
     }
 
     pub fn convert_bst(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
